0995d5b968a6282e72625121f1a3941749331bb4
Eugenio Benito <eugeniobenito00@gmail.com>
2022-10-21 00:20:25 +0200

    Connect to provisional database

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { dbConnectSqlite } = require("./config/database");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-const port = 3000
+dbConnectSqlite();
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Escuchando por el puerto ${port}`)
-})
+    console.log(`Escuchando por el puerto ${port}`);
+});
